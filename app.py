3e748e7017d2260a2dfadd3a352ea9aa6be095c8
Emyjakarta <ukonud4@gmail.com>
2024-06-17 17:23:14 +0100

    modified port and host details on app.py

--- app.py
+++ app.py
@@ -44,5 +44,7 @@ def create_tables():
 
 if __name__ == '__main__':
     create_tables()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+    # app.run(debug=True)
 
