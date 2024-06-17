70447f766242d9087ca71c71d94883280a9bf780
Emyjakarta <ukonud4@gmail.com>
2024-06-17 10:23:28 +0100

    Resolved errors in config file

--- config/database.py
+++ config/database.py
@@ -4,7 +4,7 @@ import os
 db = SQLAlchemy()
 
 def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'mysql://username:password@localhost/dbname')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
